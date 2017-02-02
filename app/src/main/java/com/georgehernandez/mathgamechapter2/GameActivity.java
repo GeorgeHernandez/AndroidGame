7d10373c9f746e4e5f9287c15f2a1f722adea1a2
George Hernandez <georgelhernandez@gmail.com>
2017-02-02 15:49:04 -0600

    Tweaked editor preferences for code styling.

--- app/src/main/java/com/georgehernandez/mathgamechapter2/GameActivity.java
+++ app/src/main/java/com/georgehernandez/mathgamechapter2/GameActivity.java
@@ -1,10 +1,12 @@
 package com.georgehernandez.mathgamechapter2;
 
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
+// Warning:(8, 35) ActionBarActivity is deprecated; use `AppCompatActivity` instead
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 
-public class GameActivity extends AppCompatActivity {
-
+//public class MainActivity extends AppCompatActivity {
+public class GameActivity extends ActionBarActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
