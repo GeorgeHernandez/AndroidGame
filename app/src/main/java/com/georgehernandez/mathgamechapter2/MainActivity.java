59c62de3fe21dbdbf9760b9f6b5f6fa5a24e4439
George Hernandez <georgelhernandez@gmail.com>
2017-02-02 15:08:16 -0600

    Initial commit.

---
+++ app/src/main/java/com/georgehernandez/mathgamechapter2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.georgehernandez.mathgamechapter2;
+
+//import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+//public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
