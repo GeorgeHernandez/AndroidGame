35aa2fbb12e5e65e8ddb2e4626b3168b942a5279
George Hernandez <georgelhernandez@gmail.com>
2017-02-03 11:59:11 -0600

    Set game variables.

--- app/src/main/java/com/georgehernandez/mathgamechapter2/GameActivity.java
+++ app/src/main/java/com/georgehernandez/mathgamechapter2/GameActivity.java
@@ -1,13 +1,32 @@
 package com.georgehernandez.mathgamechapter2;
 
 // Warning:(8, 35) ActionBarActivity is deprecated; use `AppCompatActivity` instead
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_game);
+
+    int partA = 9;
+    int partB = 9;
+    int correctAnswer = partA * partB;
+    int wrongAnswer1 = correctAnswer - 1;
+    int wrongAnswer2 = correctAnswer + 1;
+
+    TextView textObjectPartA = (TextView)findViewById(R.id.textPartA);
+    TextView textObjectPartB = (TextView)findViewById(R.id.textPartB);
+    Button buttonObjectChoice1 = (Button)findViewById(R.id.buttonChoice1);
+    Button buttonObjectChoice2 = (Button)findViewById(R.id.buttonChoice2);
+    Button buttonObjectChoice3 = (Button)findViewById(R.id.buttonChoice3);
+
+    buttonObjectChoice1.setText("" + correctAnswer);
+    buttonObjectChoice2.setText("" + wrongAnswer1);
+    buttonObjectChoice3.setText("" + wrongAnswer2);
   }
 }
