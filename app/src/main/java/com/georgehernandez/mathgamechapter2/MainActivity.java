8f133cee542d1ff5375e0f5624edfe389d3fe50a
George Hernandez <georgelhernandez@gmail.com>
2017-02-03 12:35:32 -0600

    Hooked up main page to game page.

--- app/src/main/java/com/georgehernandez/mathgamechapter2/MainActivity.java
+++ app/src/main/java/com/georgehernandez/mathgamechapter2/MainActivity.java
@@ -1,13 +1,27 @@
 package com.georgehernandez.mathgamechapter2;
 
 // Warning:(8, 35) ActionBarActivity is deprecated; use `AppCompatActivity` instead
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    Button buttonPlay = (Button)findViewById(R.id.buttonPlay);
+    buttonPlay.setOnClickListener(this);
+  }
+
+  @Override
+  public void onClick(View view) {
+    Intent i;
+    i = new Intent(this, GameActivity.class);
+    startActivity(i);
   }
 }
