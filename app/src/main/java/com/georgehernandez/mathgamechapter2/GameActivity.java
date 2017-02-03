55ff067524fa313662cd76896fc85ab4a4bd145a
George Hernandez <georgelhernandez@gmail.com>
2017-02-03 10:37:45 -0600

    Changed from ActionBarActivity to AppCompatActivity.

--- app/src/main/java/com/georgehernandez/mathgamechapter2/GameActivity.java
+++ app/src/main/java/com/georgehernandez/mathgamechapter2/GameActivity.java
@@ -1,12 +1,11 @@
 package com.georgehernandez.mathgamechapter2;
 
-//import android.support.v7.app.AppCompatActivity;
 // Warning:(8, 35) ActionBarActivity is deprecated; use `AppCompatActivity` instead
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 //public class MainActivity extends AppCompatActivity {
-public class GameActivity extends ActionBarActivity{
+public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/georgehernandez/mathgamechapter2/MainActivity.java
+++ app/src/main/java/com/georgehernandez/mathgamechapter2/MainActivity.java
@@ -1,14 +1,13 @@
 package com.georgehernandez.mathgamechapter2;
 
-//import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.ActionBarActivity;
+// Warning:(8, 35) ActionBarActivity is deprecated; use `AppCompatActivity` instead
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-//public class MainActivity extends AppCompatActivity {
-public class MainActivity extends ActionBarActivity{
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+public class MainActivity extends AppCompatActivity{
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
 }
